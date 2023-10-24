82e3606bd4e596dcbe7f4a3d032404d7bd50ee11
valesskablanco <valesskablanco@estudiantec.cr>
2023-10-23 22:03:07 -0600

    gitignore

--- patron_mvc/modelo/pruebas-modelo.py
+++ patron_mvc/modelo/pruebas-modelo.py
@@ -19,6 +19,20 @@ print("Resultado: ", modelo_calculadora.resultado)
 
 # Guarda un valor en memoria
 modelo_calculadora.guardar_memoria(1144)
+# Guarda un valor en memoria
+modelo_calculadora.guardar_memoria(4)
+print(modelo_calculadora.memoria)
+# Guarda un valor en memoria
+modelo_calculadora.guardar_memoria(5)
+print(modelo_calculadora.memoria)
+# Guarda un valor en memoria
+modelo_calculadora.guardar_memoria(6)
+print(modelo_calculadora.memoria)
+# Guarda un valor en memoria
+modelo_calculadora.guardar_memoria(2)
+print(modelo_calculadora.memoria)
+# Guarda un valor en memoria
+modelo_calculadora.guardar_memoria(1144)
 
 # Guarda un valor en memoria
 modelo_calculadora.guardar_memoria(4)
@@ -31,4 +45,7 @@ modelo_calculadora.guardar_memoria(6)
 print(modelo_calculadora.memoria)
 # Guarda un valor en memoria
 modelo_calculadora.guardar_memoria(2)
+print(modelo_calculadora.memoria)
+
+modelo_calculadora.guardar_memoria(1000)
 print(modelo_calculadora.memoria)
