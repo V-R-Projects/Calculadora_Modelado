128b36341879758b36321af78edca0ab0eb3d3fa
Ramsensei <ramsensei@estudiantec.cr>
2023-10-25 21:56:25 -0600

    controller bind

---
+++ patron_mvc/controlador/controllador.py
@@ -0,0 +1,25 @@
+
+
+class Controller:
+
+    def __init__(self, model_, view_):
+        self.model = model_
+        self.view = view_
+
+    def bindButtons(self):
+        for i in range(len(self.view.numBtns)):
+            self.view.numBtns[i].bind('<Button-1>', self.btnPress)
+        for i in range(len(self.view.opBtns)):
+            self.view.opBtns[i].bind('<Button-1>', )
+
+        self.view.btnAvg.bind('<Button-1>', )
+        self.view.btnPrimo.bind('<Button-1>', )
+        self.view.btnBinario.bind('<Button-1>', )
+        self.view.btnData.bind('<Button-1>', )
+        self.view.btnMp.bind('<Button-1>', )
+        self.view.btnEqual.bind('<Button-1>', )
+
+    def btnPress(self, ev):
+        btnStr = ev.widget.cget('text')
+        if btnStr.isdigit() or btnStr == '.':
+            self.view.txtMain.insert(len(self.view.txtMain.get()), btnStr)
