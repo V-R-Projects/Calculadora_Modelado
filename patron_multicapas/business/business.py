5d89ae8177d04f42e5dfb7965f1a5c264ce0409b
Ramsensei <ramsensei@estudiantec.cr>
2023-10-26 21:23:29 -0600

    imports

--- patron_multicapas/business/business.py
+++ patron_multicapas/business/business.py
@@ -1,4 +1,20 @@
-from ..data import data
+import sys
+from pathlib import Path
+
+if __package__ is None:
+    file = Path(__file__).resolve()
+    parent, top = file.parent, file.parents[2]
+
+    sys.path.append(str(top))
+    try:
+        sys.path.remove(str(parent))
+    except ValueError: # Already removed
+        pass
+
+    import patron_multicapas.business
+    __package__ = 'patron_multicapas.business'
+
+from ..data.data import Data
 
 class Business:
     def __init__(self, dataHandler_):
--- patron_multicapas/gui/GUI.py
+++ patron_multicapas/gui/GUI.py
@@ -1,5 +1,21 @@
 import tkinter as tk
+import sys
+from pathlib import Path
 
+if __package__ is None:
+    file = Path(__file__).resolve()
+    parent, top = file.parent, file.parents[2]
+
+    sys.path.append(str(top))
+    try:
+        sys.path.remove(str(parent))
+    except ValueError: # Already removed
+        pass
+
+    import patron_multicapas.gui
+    __package__ = 'patron_multicapas.gui'
+
+from ..business.business import Business
 
 class GUI:
     def __init__(self):
